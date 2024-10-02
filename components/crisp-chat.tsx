df3103941f4b5c5fafc1b72c9e1c5801ec9b7b66
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2024-10-02 14:56:19 +0200

    fix: adjust the key and inform where to update the api key

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,8 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
     useEffect(() => {
-        Crisp.configure("ca5f304c-343b-4cd4-8660-23c8743bae8a");
+        // todo : Add the key of crisp here
+        Crisp.configure("YOUR_KEY");
     }, []);
 
     return null;
